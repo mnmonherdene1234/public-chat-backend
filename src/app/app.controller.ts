6e979e8922edc0f1d656b24e0d7bd73afabdf846
Mn Monherdene <mnmonherdene1234@gmail.com>
2022-09-26 00:19:01 +0800

    file upload working

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { v2 as cloudinary } from 'cloudinary';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,18 @@ export class AppController {
   @Get()
   getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('upload')
+  async upload() {
+    const timestamp = Math.round(new Date().getTime() / 1000);
+    const signature = cloudinary.utils.api_sign_request(
+      {
+        timestamp,
+      },
+      process.env.CLOUDINARY_API_SECRET,
+    );
+
+    return { timestamp, signature };
   }
 }
