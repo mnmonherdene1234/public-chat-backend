ed6130bc3128ee151b7dc4c38cb60b043767b172
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2022-09-27 12:48:26 +0800

    Hello World!

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -15,9 +15,7 @@ export class AppController {
   async upload() {
     const timestamp = Math.round(new Date().getTime() / 1000);
     const signature = cloudinary.utils.api_sign_request(
-      {
-        timestamp,
-      },
+      { timestamp },
       process.env.CLOUDINARY_API_SECRET,
     );
 
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): any {
-    return { data: 'Hello' };
+  async getHello(): Promise<string> {
+    return 'Hello World!';
   }
 }
