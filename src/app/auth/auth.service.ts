2e6ef1866fd2580d50c73a72e80439c0a82a6c2f
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2022-09-28 12:38:07 +0800

    cleanup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,7 +2,6 @@ import {
   BadRequestException,
   Injectable,
   UnauthorizedException,
-  Inject,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
